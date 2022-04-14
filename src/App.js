67686e074294c4d81eb21a7a2c69b5528e47b3e8
TrinaNix4 <98623374+TrinaNix4@users.noreply.github.com>
2022-04-14 10:49:16 -0600

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Container, Header } from 'semantic-ui-css/semantic.min.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>About Trina</Header>
+      <p>resume!</p>
+    </Container>
   );
 }
 
 export default App;
+
